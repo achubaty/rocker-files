f81481df08f977a28c91eb022df08072b440d29c
Alex Chubaty <achubaty@for-cast.ca>
2025-02-28 15:19:33 -0700

    docker: force apt update before nodejs install + use v22lts

--- scripts/install_geospatial_extras.sh
+++ scripts/install_geospatial_extras.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-# always set this for scripts but don't declare as ENV..
+## always set this for scripts but don't declare as ENV..
 export DEBIAN_FRONTEND=noninteractive
 
-# a function to install apt packages only if they are not installed
+## a function to install apt packages only if they are not installed
 function apt_install() {
     if ! dpkg -s "$@" >/dev/null 2>&1; then
         if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
@@ -14,13 +14,16 @@ function apt_install() {
     fi
 }
 
-# install nodejs + mapshaper
+## install nodejs + mapshaper
 apt-get remove -y libnode-dev
-curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-apt_install nodejs
-npm install npm@latest -g
+curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+# apt_install nodejs
+apt-get update
+apt-get install -y nodejs
+
+npm install -g npm@latest
 npm install -g mapshaper
 
-# Clean up
+## clean up
 rm -rf /var/lib/apt/lists/*
 

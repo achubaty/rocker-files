634e61906f118892acebc05090830984c16c7483
Alex Chubaty <achubaty@for-cast.ca>
2026-03-30 20:47:05 -0600

    minor comment

--- scripts/config_git.sh
+++ scripts/config_git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# always set this for scripts but don't declare as ENV..
+# always set this for scripts but don't declare as ENV
 export DEBIAN_FRONTEND=noninteractive
 
 git config --global url.ssh://git@github.com/.insteadOf https://github.com/
